97babd14d4551f956b4e8966e4794d41b29ab656
Jaclyn Karnowski <jaclyn.karnowski@gmail.com>
2016-07-17 15:00:31 -0700

    removes comments

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var path = require('path');
 var bodyParser = require("body-parser");
 var app = express();
 var mongodb = require('mongodb');
-var mongoUri = 'mongodb://wriggle-admin:wrigglekids@ds055555.mlab.com:55555/wriggle-kids';
+var mongoUri = process.env.MLAB_URI;
 var ObjectID = mongodb.ObjectID;
 
 var PLAYDATES_COLLECTION = "playdates";
@@ -12,10 +12,6 @@ app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 
 var db;
-// THROUGH mLAB
-// mongodb://wriggle-admin:wrigglekids@ds055555.mlab.com:55555/wriggle-kids
-// HEROKU
-// mongodb://heroku_zzvp10k6:rhqt0gq8i9jck2v4duqqinu7bo@ds011902.mlab.com:11902/heroku_zzvp10k6
 mongodb.connect(process.env.MONGODB_URI || mongoUri, function (err, database){
   if (err) {
     console.log(err);
