13e81c9cd2fc3fabc3af3af5bf67b31ca58449be
Jaclyn Karnowski <jaclyn.karnowski@gmail.com>
2016-07-19 20:48:51 -0700

    removes comments

--- public/js/controllers/Playdates.js
+++ public/js/controllers/Playdates.js
@@ -9,34 +9,4 @@ angular.module('wriggleApp', ['ngRoute'])
       console.log('response', response)
     })
   }
-  // var playdates = [
-  // {
-  //   "title": "Swim at Lake MI" ,
-  //   "host": "Christina Woods",
-  //   "phoneNumbers": {
-  //     "mobile": "810-111-2233",
-  //   },
-  //   "location": {
-  //     "address": "111 Michigan Ave",
-  //     "city": "Chicago",
-  //     "state": "IL"
-  //   }
-  // },
-  // {
-  //   "title": "Nipples for Tots" ,
-  //   "host": "Mason Bottles",
-  //   "phoneNumbers": {
-  //     "mobile": "415-333-1212",
-  //   },
-  //   "location": {
-  //     "address": "312 Belmont Ave",
-  //     "city": "Chicago",
-  //     "state": "IL"
-  //   }
-  // }
-  // ]
-  // $scope.playdateList = playdates;
-// $scope.tagline = 'can i show you something?'
-  // $scope.playdates = response.data;
-  // console.log('playdates from ctrl', $scope.playdates);
 }])
